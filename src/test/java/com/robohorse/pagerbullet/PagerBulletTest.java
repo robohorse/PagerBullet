a8a04ad3d45f980acbea0417b160ac144b530744
Vadim Shchenev <robohorsed@gmail.com>
2017-01-28 20:29:36 +0300

    bullet test added

--- src/test/java/com/robohorse/pagerbullet/PagerBulletTest.java
+++ src/test/java/com/robohorse/pagerbullet/PagerBulletTest.java
@@ -1,8 +1,65 @@
 package com.robohorse.pagerbullet;
 
+import android.app.Activity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
 /**
  * Created by vadim on 28.01.17.
  */
-
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
 public class PagerBulletTest {
+    private PagerBullet pagerBullet;
+    private Activity activity;
+    private LinearLayout dotsLayout;
+    private TextView textIndicator;
+
+    @Before
+    public void setUp() {
+        activity = Robolectric.buildActivity(Activity.class).create().get();
+        pagerBullet = new PagerBullet(activity);
+        dotsLayout = (LinearLayout) pagerBullet.findViewById(R.id.pagerBulletIndicator);
+        textIndicator = (TextView) pagerBullet.findViewById(R.id.pagerBulletIndicatorText);
+    }
+
+    @Test
+    public void testInitialization() {
+        Assert.assertNotNull(pagerBullet);
+        Assert.assertNotNull(dotsLayout);
+        Assert.assertNotNull(textIndicator);
+        Assert.assertTrue(pagerBullet.getVisibility() == View.VISIBLE);
+    }
+
+    @Test
+    public void testDotsVisibility() {
+        final int dotsLimit = 6;
+        final int dotsCount = dotsLimit - 1;
+        pagerBullet.setTextSeparatorOffset(dotsLimit);
+        pagerBullet.setAdapter(new TestPagerAdapter(activity, dotsCount));
+
+        Assert.assertTrue(dotsLayout.getVisibility() == View.VISIBLE);
+        Assert.assertTrue(dotsLayout.getChildCount() == dotsCount);
+        Assert.assertTrue(textIndicator.getVisibility() == View.INVISIBLE);
+    }
+
+    @Test
+    public void testTextIndicatorVisibility() {
+        final int dotsLimit = 6;
+        final int dotsCount = dotsLimit + 1;
+        pagerBullet.setTextSeparatorOffset(dotsLimit);
+        pagerBullet.setAdapter(new TestPagerAdapter(activity, dotsCount));
+
+        Assert.assertTrue(dotsLayout.getVisibility() == View.INVISIBLE);
+        Assert.assertTrue(textIndicator.getVisibility() == View.VISIBLE);
+    }
 }
---
+++ src/test/java/com/robohorse/pagerbullet/TestPagerAdapter.java
@@ -0,0 +1,44 @@
+package com.robohorse.pagerbullet;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+/**
+ * Created by vadim on 28.01.17.
+ */
+
+public class TestPagerAdapter extends PagerAdapter {
+    private final Context context;
+    private final int count;
+
+    public TestPagerAdapter(Context context, int count) {
+        this.context = context;
+        this.count = count;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup collection, int position) {
+        final FrameLayout frameLayout = new FrameLayout(context);
+        collection.addView(frameLayout);
+        return frameLayout;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup collection, int position, Object view) {
+        collection.removeView((View) view);
+    }
+
+    @Override
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+}
+
